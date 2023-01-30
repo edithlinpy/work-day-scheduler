78a4dba1e890b27a0480f88a4612a81aa5e07b78
Edith <edithlinpy@gmail.com>
2023-01-30 18:30:41 +0000

    added logic to handle form submit

--- js/script.js
+++ js/script.js
@@ -1,11 +1,21 @@
 const currentDayEl = $('#currentDay');
+const timeblocksEl = $('#timeblocks');
+
 const formEl = $('#event-form');
 const now = moment();
 const currentHour = parseInt(now.format("H"));
 
-currentDayEl.text(now.format('dddd, MMMM Do'));
+const currentDay = now.format('dddd, MMMM Do');
+currentDayEl.text(currentDay);
 
-hourArray = new Array(24);
+// check if data from the previous day exist, if yes, erase all keys
+let storedCurrentDay = localStorage.getItem('currentDay');
+if (storedCurrentDay !== null && storedCurrentDay !== currentDay) {
+    localStorage.clear();
+}
+
+let hourArray = new Array(24);
+let setCurrentDay = false;
 
 // set up bootstrap grid for timeblocks
 let bgColorClass = "past";
@@ -15,7 +25,7 @@ for (let i=0; i<hourArray.length; i++) {
     } else if (i >= currentHour) {
         bgColorClass = "future";
     }
-    let hourText = moment().hour(i).format("hA"); // set hour in 12-hour AM/PM format
+    let hourText = moment().hour(i).format("hA"); // set hour in 12-hour AM/PM format    
 
     // create div for Hour column
     let rowDiv = $('<div>');
@@ -31,6 +41,7 @@ for (let i=0; i<hourArray.length; i++) {
     // create textarea element for inputing event
     let textareaEl = $('<textarea>');
     textareaEl.addClass('form-control bg-transparent');
+    textareaEl.attr('data-ta-hour', hourText);
     textareaEl.attr('rows', 3);
     textareaEl.attr('maxlength', 100);
     textareaEl.attr('data-hour', i);
@@ -44,10 +55,33 @@ for (let i=0; i<hourArray.length; i++) {
     let saveBtn = $('<button>');
     saveBtn.addClass('btn btn-primary');
     saveBtn.attr('data-toggle', 'button');
+    saveBtn.attr('type', 'submit');
     saveBtn.text('Save');
     saveDiv.append(saveBtn);
     rowDiv.append(saveDiv);
 
+    // create a form for each hour
+    let formEl = $('<form>');
+    formEl.attr('data-form-hour', hourText);
     formEl.append(rowDiv);
+
+    // append the form to the timeblocks container
+    timeblocksEl.append(formEl);
+
+    formEl.submit(function( event ) {
+        event.preventDefault();
+
+        if (!setCurrentDay) {
+            localStorage.setItem('currentDay', currentDay); // set today's date to localStorage
+            setCurrentDay = true;
+        }
+
+        let val = $('[data-ta-hour=' + hourText + ']').val().trim(); // get textarea value
+        console.log("hourText: "+hourText);
+        console.log("textarea value: "+val);
+        localStorage.setItem(hourText, val); // set textarea value to localStorage
+    });
+
+
 
 }
